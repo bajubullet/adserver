d7652f7e43e5e45bad127981424952176262692e
Abhishek Bajpai <abhishek@cargobase.com>
2018-10-21 11:20:39 +0530

    Add basic file parsing and API server

---
+++ adserver.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type AdDetail struct {
+	ID             string    `json:"id"`
+	Price          float32   `json:"price"`
+	ExpirationDate time.Time `json:"expiration_date"`
+}
+
+func main() {
+	data := readCSV()
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		adID := strings.TrimPrefix(r.URL.Path, "/promotions/")
+		if val, ok := data[adID]; ok {
+			jsonData, err := json.Marshal(val)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jsonData)
+		} else {
+			http.Error(w, "Ad not found", http.StatusNotFound)
+		}
+	}
+	http.HandleFunc("/promotions/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func readCSV() map[string]AdDetail {
+	data := make(map[string]AdDetail)
+	csvFile, err := os.Open("data.csv")
+	if err != nil {
+		log.Fatal("Error opening file", err)
+	}
+	reader := csv.NewReader(csvFile)
+	for {
+		line, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		price, err := strconv.ParseFloat(line[1], 32)
+		if err != nil {
+			log.Fatal(err)
+		}
+		expirationDate, err := time.Parse(time.RFC3339, line[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		data[line[0]] = AdDetail{
+			ID:             line[0],
+			Price:          float32(price),
+			ExpirationDate: expirationDate,
+		}
+	}
+	return data
+}
